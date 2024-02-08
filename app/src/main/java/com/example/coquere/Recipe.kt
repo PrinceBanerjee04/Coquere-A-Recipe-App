da88e0a15a21635c46311d807dd31dc9dbf9a03d
Prince <banerjee.prince69@gmail.com>
2024-02-09 00:16:01 +0530

    Update

---
+++ app/src/main/java/com/example/coquere/Recipe.kt
@@ -0,0 +1,18 @@
+package com.example.coquere
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "recipe")
+class Recipe(
+    var img:String,
+    var title:String,
+    var des:String,
+    var ing:String,
+    var catergory:String
+) {
+    @JvmField
+    @PrimaryKey(autoGenerate = true)
+    var uid=0
+
+}
